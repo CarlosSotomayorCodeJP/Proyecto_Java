79596d9c642835088fa523af9d4564ac84b4b4a2
CarlosSotomayorCodeJP <carlosmanuelsotomayorbeltran@gmail.com>
2024-09-21 22:00:39 -0400

    primer commit

--- src/main/java/com/mycompany/proyecto_java/Proyecto_Java.java
+++ src/main/java/com/mycompany/proyecto_java/Proyecto_Java.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.proyecto_java;
 
-/**
- *
- * @author PC
- */
 public class Proyecto_Java {
 
     public static void main(String[] args) {
