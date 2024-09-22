ac05cc88839969ecacb301efc8488449d2e1f275
CarlosSotomayorCodeJP <carlosmanuelsotomayorbeltran@gmail.com>
2024-09-21 21:43:32 -0400

    Iinicializacion del proyecto

---
+++ src/main/java/com/mycompany/proyecto_java/Proyecto_Java.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_java;
+
+/**
+ *
+ * @author PC
+ */
+public class Proyecto_Java {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
